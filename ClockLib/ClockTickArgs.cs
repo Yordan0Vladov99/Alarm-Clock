bb87387a50a6d81a94cbee88ee851c79f2cc087b
Yordan Vladov <43996329+Yordan0Vladov99@users.noreply.github.com>
2022-03-13 13:55:11 +0200

    added library

---
+++ ClockLib/ClockTickArgs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClockLib
+{
+    public class ClockTickArgs : EventArgs
+    {
+        private (int,int,int) clockTick;
+        public (int,int,int) ClockTick
+        {
+            get
+            {
+                return clockTick;
+            }
+            set
+            {
+                clockTick = value;
+            }
+        }
+
+        public ClockTickArgs(string str)
+        {
+            string[] args = str.Split(':');
+
+            clockTick = (int.Parse(args[0]), int.Parse(args[1]), int.Parse(args[2]));
+        }
+
+    }
+}
---
+++ ClockLib/DigitalClock.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace ClockLib
+{
+    /// <summary>
+    /// Interaction logic for DigitalClock.xaml
+    /// </summary>
+    public partial class DigitalClock : UserControl
+    {
+        public event EventHandler ClockStarted;
+        public event EventHandler TimeUpdated;
+        public DispatcherTimer timer;
+
+        public DigitalClock()
+        {
+            timer = new DispatcherTimer();
+            this.timer.Tick += new EventHandler(this.OnTimedEvent);
+            this.timer.Interval = new TimeSpan(0, 0, 1);
+
+            InitializeComponent();
+        }
+
+        private void OnTimedEvent(object source, EventArgs args)
+        {
+            DateTime now = DateTime.Now;
+            int hh = now.Hour;
+            int mm = now.Minute;
+            int ss = now.Second;
+            this.timeTxt.Text = string.Format("{0:D2}:{1:D2}:{2:D2}", hh, mm, ss);
+            TimeUpdated?.Invoke(this,
+                new ClockTickArgs(timeTxt.Text));
+        }
+
+        private void StartTimer(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
+            ClockStarted?.Invoke(this,
+                new ClockTickArgs(timeTxt.Text));
+        }
+        private void StopTimer(object sender, RoutedEventArgs e)
+        {
+            this.timer.Stop();
+        }
+        private void ResetTimer(object sender, RoutedEventArgs e)
+        {
+            timeTxt.Text = "";
+        }
+    }
+}
